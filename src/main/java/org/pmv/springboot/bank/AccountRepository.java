429fdfbfa15231e063d667f559883d5c8a86c7d4
caizo <caizo@outlook.es>
2022-03-14 19:46:57 +0100

    AccountRepository integration jpa

--- src/main/java/org/pmv/springboot/bank/AccountRepository.java
+++ src/main/java/org/pmv/springboot/bank/AccountRepository.java
@@ -1,9 +1,14 @@
 package org.pmv.springboot.bank;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    @Query("select a from Account a where a.holder=?1")
+    Optional<Account> findByHolderr(String holder);
 
 }
---
+++ src/test/java/org/pmv/springboot/bank/AccountRepositoryJpaIntegrationTest.java
@@ -0,0 +1,88 @@
+package org.pmv.springboot.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class AccountRepositoryJpaIntegrationTest {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+
+    @Test
+    void find_by_id_test() {
+        Optional<Account> account = accountRepository.findById(1L);
+        assertTrue(account.isPresent());
+        assertEquals("Juan",account.orElseThrow().getHolder());
+    }
+
+    @Test
+    void find_by_holder_test() {
+        Optional<Account> account = accountRepository.findByHolderr("Jose");
+        assertTrue(account.isPresent());
+        assertEquals("Jose",account.orElseThrow().getHolder());
+        assertEquals("2000.00", account.orElseThrow().getBalance().toPlainString());
+    }
+
+    @Test
+    void find_by_holder_throw_exception_test() {
+        Optional<Account> account = accountRepository.findByHolderr("xxx");
+        assertThrows(NoSuchElementException.class, account::orElseThrow);
+        assertFalse(account.isPresent());
+    }    
+    @Test
+    void find_all_test() {
+        List<Account> all = accountRepository.findAll();
+        assertFalse(all.isEmpty());
+        assertEquals(2,all.size());
+    }
+
+
+    @Test
+    void save_test() {
+        // GIVEN
+        Account pepeAccount = new Account(3L,"Pepe",new BigDecimal("3000"));
+        // WHEN
+        Account account = accountRepository.save(pepeAccount);
+        // THEN
+        assertEquals("Pepe",account.getHolder());
+        assertEquals("3000", account.getBalance().toPlainString());
+    }
+
+    @Test
+    void update_test() {
+        // GIVEN
+        Account pepeAccount = new Account(3L,"Pepe",new BigDecimal("3000"));
+        // WHEN
+        Account account = accountRepository.save(pepeAccount);
+        // THEN
+        assertEquals("Pepe",account.getHolder());
+        assertEquals("3000", account.getBalance().toPlainString());
+
+        // WHEN
+        account.setBalance(new BigDecimal("4000"));
+        Account updatedAccount = accountRepository.save(account);
+        // THEN
+        assertEquals("4000", updatedAccount.getBalance().toPlainString());
+
+    }
+
+
+    @Test
+    void delete_test() {
+        Account account = accountRepository.findById(1L).orElseThrow();
+        assertEquals("Juan", account.getHolder());
+        accountRepository.delete(account);
+
+        assertThrows(NoSuchElementException.class, () -> accountRepository.findByHolderr("Juan").orElseThrow());
+    }
+}
